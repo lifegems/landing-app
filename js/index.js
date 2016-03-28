6f3cf2c67ee5d62d0bf738d1d347d1b7471b46a3
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-03-28 14:28:55 +0000

    Add animation to page on load

---
+++ js/index.js
@@ -0,0 +1,17 @@
+(function() {
+   var tl = new TimelineMax();
+   tl.from("#pageTitle", 1, {x: "-=5000", ease: Elastic.easeOut});
+   tl.from("#p-item-1", 0.7, {x: "-=5000", ease: Expo.easeOut}, 0.5);
+   tl.from("#p-item-2", 0.7, {y: "-=5000", ease: Expo.easeOut}, 0.6);
+   tl.from("#p-item-3", 0.7, {x: "+=5000", ease: Expo.easeOut}, 0.7);
+   tl.from("#p-item-4", 0.7, {y: "+=5000", ease: Expo.easeOut}, 0.8);
+
+   $(".portfolio-item").hover(function(e) {
+      TweenMax.to($(this), 0.5, {scale: 1.1});
+   }, function(e) {
+      TweenMax.to($(this), 0.5, {scale: 1});
+   });
+   $(".portfolio-item").on('click', function(e) {
+      TweenMax.to($(this), 0.3, {scale: 0.5, x: "+=500", y: "-=500"});
+   });
+})();
