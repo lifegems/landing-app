2e39711057526a21feaef639fa36cafa47b431d4
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-03-29 14:49:14 +0000

    Add link for 10 plagues

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@
    tl.from("#p-item-4", 0.7, {y: "+=5000", ease: Expo.easeOut}, 0.8);
    tl.from("#p-item-5", 0.7, {y: "+=5000", ease: Expo.easeOut}, 0.9);
    tl.from("#p-item-6", 0.7, {x: "+=5000", ease: Expo.easeOut}, 1);
+   tl.from("#p-item-7", 0.7, {x: "-=5000", ease: Expo.easeOut}, 1.1);
 
    $(".portfolio-item").hover(function(e) {
       TweenMax.to($(this), 0.5, {scale: 1.1});
