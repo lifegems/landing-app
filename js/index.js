d929bad4143b45446f3bb654c6ae99d6fecb0fc4
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-03-30 19:34:27 +0000

    Add offerings app

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@
    tl.from("#p-item-5", 0.7, {y: "+=5000", ease: Expo.easeOut}, 0.9);
    tl.from("#p-item-6", 0.7, {x: "+=5000", ease: Expo.easeOut}, 1);
    tl.from("#p-item-7", 0.7, {x: "-=5000", ease: Expo.easeOut}, 1.1);
+   tl.from("#p-item-8", 0.7, {y: "+=5000", ease: Expo.easeOut}, 1.2);
 
    $(".portfolio-item").hover(function(e) {
       TweenMax.to($(this), 0.5, {scale: 1.1});
