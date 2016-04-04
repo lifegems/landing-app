86c495d39457686644911eafcd33ea9edab72b64
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-04-04 15:21:46 +0000

    Add font awesome icons, update menu icon

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
 (function () {
    'use strict';
-   
+
    var isMenuExpanded = false;
-   
+
    $(".mainmenu--expand").click(function(e) {
       if (isMenuExpanded === false) {
-         $(this).text("HIDE");
+         $(this).html("<i class='fa fa-chevron-up fa-2x'></i>");
          $(".mainmenu--item").removeClass("is-hidden");
          isMenuExpanded = true;
       } else {
-         $(this).text("SHOW");
+         $(this).html("<i class='fa fa-bars fa-2x'></i>");
          $(".mainmenu--item").addClass("is-hidden");
          isMenuExpanded = false;
       }
    });
-   
+
    $(".date").click(function(e) {
       var setDate = $(this).text();
       $(".date.is-center").text(setDate);
       $(".date.is-left").text(parseInt(setDate) - 1);
       $(".date.is-right").text(parseInt(setDate) + 1);
-      
+
    });
-   
+
    if ( $('.mainmenu--expand').css('display') === 'block') {
       $(".mainmenu--item").addClass("is-hidden");
    }
