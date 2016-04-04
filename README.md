d354474988ed4565ab3cac77131f7aaeffd96f01
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-04-04 14:40:44 +0000

    Replace old landing page with new one

--- README.md
+++
@@ -1,2 +0,0 @@
-# Life gems landing page
-Checkout https://lifegems.github.io for a few bible study apps.
--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
-(function() {
-   var tl = new TimelineMax();
-   tl.from("#pageTitle", 1, {x: "-=5000", ease: Elastic.easeOut});
-   tl.from("#p-item-1", 0.7, {x: "-=5000", ease: Expo.easeOut}, 0.5);
-   tl.from("#p-item-2", 0.7, {y: "-=5000", ease: Expo.easeOut}, 0.6);
-   tl.from("#p-item-3", 0.7, {x: "+=5000", ease: Expo.easeOut}, 0.7);
-   tl.from("#p-item-4", 0.7, {y: "+=5000", ease: Expo.easeOut}, 0.8);
-   tl.from("#p-item-5", 0.7, {y: "+=5000", ease: Expo.easeOut}, 0.9);
-   tl.from("#p-item-6", 0.7, {x: "+=5000", ease: Expo.easeOut}, 1);
-   tl.from("#p-item-7", 0.7, {x: "-=5000", ease: Expo.easeOut}, 1.1);
-   tl.from("#p-item-8", 0.7, {y: "+=5000", ease: Expo.easeOut}, 1.2);
-
-   $(".portfolio-item").hover(function(e) {
-      TweenMax.to($(this), 0.5, {scale: 1.1});
-   }, function(e) {
-      TweenMax.to($(this), 0.5, {scale: 1});
+(function () {
+   'use strict';
+   
+   var isMenuExpanded = false;
+   
+   $(".mainmenu--expand").click(function(e) {
+      if (isMenuExpanded === false) {
+         $(this).text("HIDE");
+         $(".mainmenu--item").removeClass("is-hidden");
+         isMenuExpanded = true;
+      } else {
+         $(this).text("SHOW");
+         $(".mainmenu--item").addClass("is-hidden");
+         isMenuExpanded = false;
+      }
    });
+   
+   $(".date").click(function(e) {
+      var setDate = $(this).text();
+      $(".date.is-center").text(setDate);
+      $(".date.is-left").text(parseInt(setDate) - 1);
+      $(".date.is-right").text(parseInt(setDate) + 1);
+      
+   });
+   
+   if ( $('.mainmenu--expand').css('display') === 'block') {
+      $(".mainmenu--item").addClass("is-hidden");
+   }
 })();
