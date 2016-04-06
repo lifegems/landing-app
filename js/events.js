0347ddef5f54b3c192f122e4a3270f88c22c59c7
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-04-06 21:01:33 +0000

    Initial migration to timelinejs

---
+++ js/events.js
@@ -0,0 +1,174 @@
+var EVENTS = {};
+
+EVENTS.PATRIARCHS = [
+   {
+      "start_date": {
+         "year": "-4026"
+      },
+      "end_date": {
+         "year": "-3096"
+      },
+      "text": {
+         "headline": "Adam"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-4000",
+         "display_date": "a. 4026 BCE"
+      },
+      "end_date": {
+         "year": "-3096",
+         "display_date": "c. 3096 BCE"
+      },
+      "text": {
+         "headline": "Eve"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3896"
+      },
+      "end_date": {
+         "year": "-2984"
+      },
+      "text": {
+         "headline": "Seth"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3791"
+      },
+      "end_date": {
+         "year": "-2886"
+      },
+      "text": {
+         "headline": "Enosh"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3701"
+      },
+      "end_date": {
+         "year": "-2791"
+      },
+      "text": {
+         "headline": "Kenan"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3631"
+      },
+      "end_date": {
+         "year": "-2736"
+      },
+      "text": {
+         "headline": "Mahalalel"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3566"
+      },
+      "end_date": {
+         "year": "-2604"
+      },
+      "text": {
+         "headline": "Jared"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3404"
+      },
+      "end_date": {
+         "year": "-3039"
+      },
+      "text": {
+         "headline": "Enoch"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3339"
+      },
+      "end_date": {
+         "year": "-2370"
+      },
+      "text": {
+         "headline": "Methuselah"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-3152"
+      },
+      "end_date": {
+         "year": "-2375"
+      },
+      "text": {
+         "headline": "Lamech"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-2970"
+      },
+      "end_date": {
+         "year": "-2070"
+      },
+      "text": {
+         "headline": "Noah"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-2470"
+      },
+      "end_date": {
+         "year": "-2070"
+      },
+      "text": {
+         "headline": "Shem"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-2470"
+      },
+      "end_date": {
+         "year": "-2070"
+      },
+      "text": {
+         "headline": "Ham"
+      }
+   },
+   {
+      "start_date": {
+         "year": "-2470"
+      },
+      "end_date": {
+         "year": "-2070"
+      },
+      "text": {
+         "headline": "Japheth"
+      }
+   }
+];
+
+EVENTS.EVENTS = [
+   {
+      "start_date": {
+         "year": "-2470"
+      },
+      "end_date": {
+         "year": "-2070"
+      },
+      "text": {
+         "headline": "WHAT??"
+      }
+   }
+];
--- js/timeline.js
+++ js/timeline.js
@@ -7,145 +7,47 @@ var Timeline = (function Timeline() {
    /**
     * FUNCTIONS
     */
-   function getEvent(intID, strColor) {
-         var tag = "tag_" + intID;
-         var $newevent = $("<td></td>", {id: tag, class: "events--event"});
-         var date = "event " + intID;
-         $newevent.text(date);
-
-         $newevent.css('backgroundColor', strColor);
-
-         if (intID === 3) {
-            $newevent.attr('colspan', 2.5);
+   function listEvents() {
+      var json = {
+         "title": {
+            "text": {
+               "headline": "Lifespans of mankind",
+               "text": "From Adam until today"
+            }
          }
-
-         $newevent.click(function(e) {
-            var tag = "#" + $(this).attr('id') + "_content";
-            $('.toolbar--details').show();
-         });
-
-         return $newevent;
-   }
-
-   function getEventRow(strColor, intRowID) {
-      var $eventrow = $("<tr></tr>", {class: "events--row"});
-      for (var i = 0; i < 5; i++) {
-         var $newevent = getEvent(i, strColor);
-         $eventrow.append($newevent);
       };
-      return $eventrow;
-   }
 
-   function getDatesRow() {
-      var $dates = $("<tfoot></tfoot>", {class: ".timeline--dateline"});
-      var $line = $("<tr></tr>");
-      for (var i = 0; i < 15; i++) {
-         var $footer = $("<th></th>", {class: "date"})
-         var $date = $("<div></div>", {class: "date--span"});
-         var date = 4027 - i;
-         $date.text(date);
-         $footer.append($date);
+      json.events = EVENTS.PATRIARCHS;
 
-         $line.append($footer);
-      }
-      $dates.append($line);
-      return $dates;
+      return json;
    }
 
    function loadTimeline() {
-      var $tl = $(".timeline");
-      if ($tl.length === 1) {
-         // build date line
-         var $dates = getDatesRow();
+      var event_json = listEvents();
+      window.timeline = new TL.Timeline('timeline', event_json, {
+         "timenav_height_percentage": 90
+      });
+      hideSlider();
 
-         // build events
-         var $events = $("<tbody></tbody", {class: "events"});
-            // each event type gets it's own row
-         var colors = ["#AFC9D8", "#E8F7FF", "#BAD5E5", "#B6CBEF"];
-         for (var i = 0; i < 15; i++) {
-            var $eventsrow = getEventRow(colors[i % 4], i);
-            $events.append($eventsrow);
-         }
 
-         // add to DOM
-         $tl.append($events);
-         $tl.append($dates)
-      }
+
+      $(".tl-timemarker").on('click', function(e) {
+         $('.toolbar--details').show();
+      });
+   }
+
+   function hideSlider() {
+      $('.tl-storyslider').hide();
+
+      // hide advert
+      $('.tl-attribution').remove();
    }
 
    /**
     * EVENTS
     */
-
-   /**
-    * Search for events in the timeline
-    */
-   $("#searchTimeline").keyup(function(e) {
-      var regex = new RegExp($("#searchTimeline").val(), "i");
-      var event = $(".events--event");
-      event.each(function(index) {
-         if ($(this).html().search(regex) != -1) {
-            $(this).show();
-         } else {
-            $(this).hide();
-         }
-      });
-   });
-
    $(".details--expand").on('click', function(e) {
       $('.toolbar--details').hide();
-   })
-
-   /**
-    * Open select a year box
-    */
-   $('.toolbar--year').on('click', function(e) {
-      if ($('.toolbar--selectyear').css('display') === 'none') {
-         $('.toolbar--selectyear').css('display', 'block');
-      } else {
-         $('.toolbar--selectyear').css('display', 'none');
-      }
-   });
-
-   /**
-    * Zoom in on events to see a more detailed view
-    */
-   $('.toolbar--zoom--in').on('click', function(e) {
-      var width = $('.date--span').css('width');
-      width = width.substr(0, width.length - 2);
-      $('.date--span').css('width', width * 1.5);
-
-      var padding = $('.events--event').css('padding');
-      padding = padding.substr(0, padding.length - 2);
-      $('.events--event').css('padding', padding * 1.5);
-
-      var fontSize = $('.events--event').css('font-size');
-      fontSize = fontSize.substr(0, fontSize.length - 2);
-      $('.events--event').css('font-size', fontSize * 1.5);
-   });
-
-   /**
-    * Zoom out on events to see a broader range of events
-    */
-   $('.toolbar--zoom--out').on('click', function(e) {
-      var width = $('.date--span').css('width');
-      width = width.substr(0, width.length - 2);
-      $('.date--span').css('width', width * 0.67);
-
-      var padding = $('.events--event').css('padding');
-      padding = padding.substr(0, padding.length - 2);
-      $('.events--event').css('padding', padding * 0.67);
-
-      var fontSize = $('.events--event').css('font-size');
-      fontSize = fontSize.substr(0, fontSize.length - 2);
-      $('.events--event').css('font-size', fontSize * 0.67);
-   });
-
-   /**
-    * Select a year to navigate to
-    */
-   $('.toolbar--selectyear').on('change', function(e) {
-      $('.toolbar--selectyear').css('display', 'none');
    })
 
    return service;
